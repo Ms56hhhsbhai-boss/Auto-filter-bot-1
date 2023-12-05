9719f43de92ab7c6ee5269e578d772aa47449821
Abhi dada <152891093+Ms56hhhsbhai-boss@users.noreply.github.com>
2023-12-05 17:45:12 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/deendayal422/AUTO-FILTER-BOT.git /AUTO-FILTER-BOT 
+  git clone https://github.com/Ms56hhhsbhai-boss/AUTO-FILTER-BOT.git /AUTO-FILTER-BOT 
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /AUTO-FILTER-BOT
