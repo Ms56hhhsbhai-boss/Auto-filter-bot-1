46c474a5413fb405af6b075bd5e9ebbedf4cdd0f
Abhi dada <152891093+Ms56hhhsbhai-boss@users.noreply.github.com>
2023-12-06 13:06:23 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -179,7 +179,7 @@ async def start(client, message):
                     reply_markup=InlineKeyboardMarkup(
                         [
                          [
-                          InlineKeyboardButton("⚜️ Updates Channel ⚜️", url="t.me/Deendayal_dhakad")
+                          InlineKeyboardButton("⚜️ Updates Channel ⚜️", url="t.me/Godslayerborutoo")
                          ],[ 
                             InlineKeyboardButton('🚀 Fast Download / Watch Online🖥️', callback_data=f'generate_stream_link:{file_id}') #Don't change anything without contacting me @LazyDeveloperr
                             ]
